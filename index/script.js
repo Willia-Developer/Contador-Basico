1dd1e2efc6ea7c2e30b7ee7aaba844e543b8759d
WilliaS26 <circuitbreak2020@gmail.com>
2022-06-27 15:04:32 -0300

    index

--- index/script.js
+++ index/script.js
@@ -1,3 +1,4 @@
+
 var currentNumberWapper = document.getElementById("currentNumber")
 var currentNumber = 0
 
@@ -10,3 +11,37 @@ function decrement() {
     currentNumber = currentNumber - 1;
     currentNumberWapper.innerHTML = currentNumber;
 }
+
+function changeMode() {
+	changeClasses();
+	changeText();
+}
+
+function changeClasses() {
+	button.classList.toggle(darkModeClass);
+	h1.classList.toggle(darkModeClass);
+	body.classList.toggle(darkModeClass);
+	footer.classList.toggle(darkModeClass);
+}
+
+function changeText() {
+	const lightMode = 'Light Mode';
+	const darkMode = 'Dark Mode';
+
+	if (body.classList.contains(darkModeClass)) {
+		button.innerHTML = lightMode;
+		h1.innerHTML = darkMode + ' ON';
+		return;
+	}
+
+	button.innerHTML = darkMode;
+	h1.innerHTML = lightMode + ' ON';
+}
+
+const darkModeClass = 'dark-mode';
+const button = document.getElementById('mode-selector');
+const h1 = document.getElementById('page-title');
+const body = document.getElementsByTagName('body')[0];
+const footer = document.getElementsByTagName('footer')[0];
+
+button.addEventListener('click', changeMode);
